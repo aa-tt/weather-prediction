e6403ed40c17c94c0f8e31b80cc334818907f0b4
aa-tt <i.anunay@gmail.com>
2023-02-13 23:10:50 +0530

    kafka producer and consumer for monetizing service

--- weather-prediction-api/src/main/kotlin/com/aa/weatherprediction/service/ForecastService.kt
+++ weather-prediction-api/src/main/kotlin/com/aa/weatherprediction/service/ForecastService.kt
@@ -1,18 +1,26 @@
 package com.aa.weatherprediction.service
 
-import com.aa.weatherprediction.model.DayAndReport
-import com.aa.weatherprediction.model.Report
-import com.aa.weatherprediction.model.WeatherData
+import com.aa.weatherprediction.model.*
+import org.springframework.messaging.Message
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import kotlin.collections.ArrayList
 
 @Service
 class ForecastService(
+    private val kafkaTemplate: KafkaTemplate<String, Any>,
     private val connectorService: ConnectorService
 ) {
 
+    @Value("\${kafka.topic}")
+    private val topic: String = ""
+
     fun getForecastByCity(name: String): Report {
+        sendEvent(EVENT.ORDER)
         val city = connectorService.getCity(name).first()
         val weatherData: WeatherData = connectorService.getWeather(city.lat!!, city.lon!!)
         val weatherAlerts: ArrayList<String> = arrayListOf()
@@ -25,12 +33,22 @@ class ForecastService(
         if (weatherData.rain != null) {
             weatherAlerts.add("Carry Umbrella")
         }
+        sendEvent(EVENT.SUCCESS)
         return Report(
             alerts = weatherAlerts,
             mains = weatherData.weather?.map { it.main!! },
             temp_min = weatherData.main?.temp_min,
             temp_max = weatherData.main?.temp_max,
         )
+    }
+
+    private fun sendEvent(type: EVENT) {
+        val eventMessage: Message<UsagePrice> = MessageBuilder
+            .withPayload(UsagePrice("User1", type))
+            .setHeader(KafkaHeaders.TOPIC, topic)
+            .setHeader("X-Custom-Header", "Custom header here")
+            .build()
+        kafkaTemplate.send(eventMessage)
     }
 
     // using Mock Data as forecast service comes under Pro subscription
