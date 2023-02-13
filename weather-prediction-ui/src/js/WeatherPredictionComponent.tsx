f676dd67bd07b7c03d84db353290a24ad1ca66be
aa-tt <i.anunay@gmail.com>
2023-02-14 02:33:07 +0530

    styling history

--- weather-prediction-ui/src/js/WeatherPredictionComponent.tsx
+++ weather-prediction-ui/src/js/WeatherPredictionComponent.tsx
@@ -30,12 +30,17 @@ const WeatherPredictionComponent: FunctionComponent = () => {
 
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      {messages.map(m => (
-        <div className='flex flex-row text-sm' key={m}>
-          <div>{m}</div>
-        </div>
-      ))
-      }
+      <div className='flex flex-row text-sm'>
+        {messages.map(m => (
+          <div
+            key={m}
+            className='ml-2 mt-2 bg-white dark:bg-slate-100 rounded-sm px-2 py-2 ring-1 ring-slate-100/5 shadow-sm'
+          >
+            {m}
+          </div>
+        ))
+        }
+      </div>
       <CitySelector setCity={setCity} />
       <div className='mt-3'>
         <p className='text-gray-500 dark:text-gray-400 bold'>
